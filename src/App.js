84d71beb307d061537feb8b295bbfcff37ba06ea
Wolfgruengit <gruenberger.w@outlook.com>
2023-07-10 10:42:58 +0200

    next test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Public Test
+          hello
         </p>
         <a
           className="App-link"
